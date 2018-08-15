bb2056488a4dcb71c92f25b1247bf29b0638f8ae
forci0ne <info@peetsj.nl>
2018-08-15 20:56:22 +0200

    Writed first lines of code

--- ledpay.py
+++ ledpay.py
@@ -1,1 +1,20 @@
-// LEDPAY 0.0
+# LEDPAY version 0.1 based on a tutorial by Hugo Gregersen (Integrating physical devices with IOTA)
+
+# import some python date and time functions
+import time
+import datetime
+
+# imports the GPIO library
+import RPi.GPIO as GPIO
+
+# imports the PYOTA library
+from iota import Iota
+from iota import Address
+
+# setting up the 0/I raspberry pin's
+LEDPIN = 18
+GPIO.setmode(GPIO.BCM)
+
+
+
+
